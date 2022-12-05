a4e05ed9ddd126012907f56e10071162e237f144
Mochamad akbar <mochakbar912@gmail.com>
2022-12-05 17:59:29 +0700

    feat: streaming api

---
+++ calculator/server/primes.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+
+	pb "github.com/MochamadAkbar/go-grpc-example/calculator/proto"
+)
+
+func (s *Server) Primes(in *pb.PrimesRequest, stream pb.CalculatorService_PrimesServer) error {
+	log.Printf("Primes function was invoked with: %v\n", in)
+
+	number := in.Number
+	divisior := int32(2)
+
+	for number > 1 {
+		if number%divisior == 0 {
+			stream.Send(&pb.PrimesResponse{
+				Result: divisior,
+			})
+
+			number /= divisior
+		} else {
+			divisior++
+		}
+	}
+
+	return nil
+}
