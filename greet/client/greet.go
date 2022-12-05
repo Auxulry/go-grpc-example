5889335b5f22ee718b53a4bbfb21c6f5bdee6143
Mochamad akbar <mochakbar912@gmail.com>
2022-12-05 16:19:42 +0700

    feat: finish great grpc example

---
+++ greet/client/greet.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	pb "github.com/MochamadAkbar/go-grpc-example/greet/proto"
+)
+
+func doGreet(c pb.GreetServiceClient) {
+	log.Println("doGreet was invoked")
+
+	res, err := c.Greet(context.Background(), &pb.GreetRequest{
+		FirstName: "Akbar",
+	})
+
+	if err != nil {
+		log.Fatalf("Could not greet: %v\n", err)
+	}
+
+	log.Printf("Greeting: %s\n", res.Result)
+}
---
+++ greet/client/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	pb "github.com/MochamadAkbar/go-grpc-example/greet/proto"
+)
+
+var Arr = "localhost:50051"
+
+func main() {
+	// for development use => grpc.WithTransportCredentials(insecure.NewCredentials())
+	conn, err := grpc.Dial(Arr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	if err != nil {
+		log.Fatalf("Failed to connect: %v\n", err)
+	}
+
+	defer conn.Close()
+
+	c := pb.NewGreetServiceClient(conn)
+
+	doGreet(c)
+}
